82fe673e5f257d8feade8acb406c3ba2e4a997aa
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-04 20:42:26 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,6 @@
-import * as React from "react";
-import { useEffect, useState } from "react";
-import { createClient } from "contentful";
-
-const client = createClient({
-  space: "VOTRE_SPACE_ID",
-  accessToken: "VOTRE_ACCESS_TOKEN",
-});
+import * as React from "react"
 
 export default function Home() {
-  const [content, setContent] = useState(null);
-
-  useEffect(() => {
-    // Récupérer le contenu depuis Contentful
-    client
-      .getEntry("helloWorld")
-      .then((entry) => {
-        // Mettre à jour l'état avec le contenu récupéré
-        setContent(entry.fields);
-      })
-      .catch((error) => console.error("Erreur lors de la récupération du contenu Contentful", error));
-  }, []);
-
-  return (
-    <div>
-      {content ? (
-        <div>
-          <p>{content.helloWorld}</p>
-          <p>test</p>
-        </div>
-      ) : (
-        <p>Chargement...</p>
-      )}
-    </div>
-  );
+  return <div>Hello world! <p>test</p></div>
+ 
 }
